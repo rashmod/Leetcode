0e6b4e07b66513643e03b16d4baaee490aec1cce
Bhushan Alhat <alhatbhushan@gmail.com>
2023-07-19 20:40:52 +0530

    1984-minimum-difference-between-highest-and-lowest-of-k-scores.py

---
+++ python/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -0,0 +1,20 @@
+# Array Sliding-window Sorting
+from typing import List
+
+
+# Optimal
+# space complexity: O(nlogn + n) (for sorting: nlogn) + (if new array is created: n)
+# time complexity: O(nlogn + n) (for sorting: nlogn)
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        # if the original array should not be mutated
+        sNums = sorted(nums, reverse=True)
+        # otherwise this
+        # nums.sort(reverse=True)
+        first, last = 0, 0
+        ans = float("inf")
+        for i in range(len(sNums) - k + 1):
+            first = sNums[i]
+            last = sNums[i + k - 1]
+            ans = min(ans, first - last)
+        return ans
