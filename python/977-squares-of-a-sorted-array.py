9ac068076138ac840dd32f2476fc745c571dddaf
Bhushan Alhat <alhatbhushan@gmail.com>
2023-07-29 13:14:43 +0530

    977-squares-of-a-sorted-array.py

---
+++ python/977-squares-of-a-sorted-array.py
@@ -0,0 +1,37 @@
+# Array Two-pointer Sorting
+from typing import List
+
+# Brute force
+# space complexity: O(nlogn)
+# time complexity: O(nlogn)
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            nums[i] *= nums[i]
+
+        nums.sort()
+        return nums
+
+
+# Optimal
+# space complexity: O(n)
+# time complexity: O(n)
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        res = [None] * len(nums)
+        left, right = 0, len(nums) - 1
+        i = len(nums) - 1
+
+        while left <= right:
+            curr = None
+            if abs(nums[left]) > abs(nums[right]):
+                curr = nums[left]
+                left += 1
+            else:
+                curr = nums[right]
+                right -= 1
+
+            res[i] = curr * curr
+            i -= 1
+
+        return res
